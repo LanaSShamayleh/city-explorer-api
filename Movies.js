93f1163064f761f9b58331766349c9354c3869d9
Lana Salah Shamaileh <shamaylehlana62@gmail.com>
2021-08-12 15:41:59 +0300

    new submission

--- Movies.js
+++ Movies.js
@@ -9,28 +9,40 @@ class Movies {
     this.title = item.original_title;
     this.overview = item.overview;
     this.avgVotes = item.vote_average;
-    this.totalVotes= item.vote_count;
-    this.imagePath =`https://image.tmdb.org/t/p/w500${item.poster_path}`;
+    this.totalVotes = item.vote_count;
+    this.imagePath = `https://image.tmdb.org/t/p/w500${item.poster_path}`;
     this.popularity = item.popularity;
     this.releaseDate = item.release_date;
 
   }
 }
 
+let array = {};
 function movieHandler(req, res) {
   let city = req.query.searchQuery;
   let url = `https://api.themoviedb.org/3/search/movie?api_key=${movieKey}&query=${city}`;
-  axios.get(url).then(result => {
-    let movieArray = result.data.results.map(element => {
-      return new Movies(element);
-    });
-    res.send(movieArray);
-  })
-    .catch(error => {
-      console.log(error.response.status);
-      let e=error.response.status;
-      res.status(e).send(`data not found.${error}`);
-    });
+
+  if (array[city] !== undefined) {
+    console.log('data from memory');
+    res.send(array[city]);
+  } else {
+    console.log('data form the Movie API');
+    axios.get(url).then(result => {
+      let movieArray = result.data.results.map(element => {
+        return new Movies(element);
+      });
+
+      array[city] = movieArray;
+      res.send(movieArray);
+
+    })
+      .catch(error => {
+        console.log(error.response.status);
+        let e = error.response.status;
+        res.status(e).send(`data not found.${error}`);
+      });
+  }
+
 }
 
 module.exports = movieHandler;
